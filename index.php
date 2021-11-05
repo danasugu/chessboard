0de59524792acdb3a5252e98166c9b2c98de75bb
Dana Sugu <dana.sugu@gmail.com>
2021-11-05 16:21:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,31 @@
 </head>
 <body>
     <table>
+
+    <form method="post">
+        <select>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+        </select>
+    </form>
+    
+    <button name="submit" type="submit">Submit</button>
+ 
         <?php 
-        for($rows = 1; $rows <= 8; $rows++)
+        if(isset($_POST['submit'])){
+            $size = $_POST['size'];
+        } else{
+            $size = 8;
+        }
+        for($rows = 1; $rows <= $size; $rows++)
             {
                 echo "<tr>";
-                   for($columns = 1; $columns <= 8; $columns++)
+                   for($columns = 1; $columns <= $size; $columns++)
                    {
                        $total = $rows + $columns;
                        if($total % 2 == 0) {
